c31fb2df8927c264502b0f4e844ef0fdbbd48cd0
KimTrang0610 <vothikimtrang06101997@gmail.com>
2021-11-11 01:28:53 +0700

    reactjs-booking-movie-ticket

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import './App.css';
+import "./App.css";
 import { createBrowserHistory } from "history";
-import { Route, Router, Switch } from 'react-router';
+import { Route, Router, Switch } from "react-router";
 
-import TrangChuHome from './pages/TrangChuHome/TrangChuHome';
-import { HomeTemplate } from './templates/HomeTemplate/HomeTemplate';
-import Contact from './pages/Contact/Contact';
-import News from './pages/News/News';
-import Login from './pages/Login/Login';
-import Register from './pages/Register/Register';
-import Detail from './pages/Detail/Detail';
-import CheckoutTemplate from './templates/CheckoutTemplate/CheckoutTemplate';
-import Checkout from './pages/Checkout/Checkout';
+import TrangChuHome from "./pages/TrangChuHome/TrangChuHome";
+import { HomeTemplate } from "./templates/HomeTemplate/HomeTemplate";
+import Contact from "./pages/Contact/Contact";
+import News from "./pages/News/News";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Detail from "./pages/Detail/Detail";
+import CheckoutTemplate from "./templates/CheckoutTemplate/CheckoutTemplate";
+import Checkout from "./pages/Checkout/Checkout";
 import { Suspense, lazy } from "react";
-import { UserTemplate } from './templates/UserTemplate/UserTemplate';
-import Loading from './components/Loading/Loading';
-import Profile from './pages/Profile/Profile';
-import Dashboard from './pages/Admin/Dashboard/Dashboard';
-import Films from './pages/Admin/Films/Films';
-import AdminTemplate from './templates/AdminTemplate/AdminTemplate';
+import { UserTemplate } from "./templates/UserTemplate/UserTemplate";
+import Loading from "./components/Loading/Loading";
+import Profile from "./pages/Profile/Profile";
+import Dashboard from "./pages/Admin/Dashboard/Dashboard";
+import Films from "./pages/Admin/Films/Films";
+import AdminTemplate from "./templates/AdminTemplate/AdminTemplate";
+import AddNew from "./pages/Admin/Films/AddNew/AddNew";
 
-
-const CheckoutTemplateLazy = lazy(() => import('./templates/CheckoutTemplate/CheckoutTemplate'))
+const CheckoutTemplateLazy = lazy(() =>
+  import("./templates/CheckoutTemplate/CheckoutTemplate")
+);
 
 export const history = createBrowserHistory();
 
@@ -35,7 +37,6 @@ function App() {
         <HomeTemplate path="/detail/:id" exact Component={Detail} />
         <HomeTemplate path="/profile" exact Component={Profile} />
 
-
         <CheckoutTemplate path="/checkout/:id" exact Component={Checkout} />
 
         <UserTemplate path="/login" exact Component={Login} />
@@ -43,8 +44,7 @@ function App() {
 
         <AdminTemplate path="/admin" exact Component={Dashboard} />
         <AdminTemplate path="/admin/films" exact Component={Films} />
-        <AdminTemplate path="/admin/users" exact Component={Dashboard} />
-
+        <AdminTemplate path="/admin/films/addnew" exact Component={AddNew} />
 
         <AdminTemplate path="/admin/users" exact Component={Dashboard} />
         {/* <Suspense fallback={<h1>LOADING....</h1>}>
@@ -52,12 +52,9 @@ function App() {
         </Suspense> */}
 
         <HomeTemplate path="/" exact Component={TrangChuHome} />
-
-
       </Switch>
     </Router>
   );
 }
 
 export default App;
-
